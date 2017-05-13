d0a644e7c2b007113d9a7e5b862a2915ffa2e967
Menefrego <Menefrego>
2017-05-13 12:49:42 +0200

    idem

--- Modele/User.php
+++ Modele/User.php
@@ -3,22 +3,22 @@
 require_once("Modele/Primary.php");
 
 class User extends Primary{
-
+    
     private $firstName = "";
     private $lastName = "";
-
+    
     function __construct($id,$firstName,$lastName,$isAdmin){
         parent::__construct($id,$isAdmin);
         $this->setFirstName($firstName);
         $this->setLastName($lastName);
     }
-
-   function getFirstName(){return $this->firstName;}
+    
+    function getFirstName(){return $this->firstName;}
     function setFirstName($firstName){$this->firstName= $firstName;}
     function getLastName(){return $this->lastName;}
     function setLastName($lastName){$this->lastName= $lastName;}
     function getIsAdmin(){return $this->isAdmin;}
     function setIsAdmin($isAdmin){$this->isAdmin= $isAdmin;}
-
+    
     
 }
