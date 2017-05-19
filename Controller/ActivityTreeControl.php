b5437eff3e790bec0fca6eac8eaf1d00058bec3e
Phrederik <phrederik2@gmail.com>
2017-05-19 11:33:25 +0200

    Fin création arbre

--- Controller/ActivityTreeControl.php
+++ Controller/ActivityTreeControl.php
@@ -1,20 +1,14 @@
 <?php
 
 /**
+ * Controller qui sert a gerer la création de l'arbre des activity  (multi-niveau)
  * 
  */
 class ActivityTree 
 {
-    private $dbco;
-    
-    function __construct()
-    {
-        $this->dbco = DbCo::getPDO();
-    }
-
-    function getData(){
-
-        $query = 
+    private $dbco = null;
+    private $data = Array();
+    private $query = 
         '
         select department.TITLE department, curriculum.TITLE curriculum, teachingunit.TITLE teachingunit, activityunit.TITLE activityunit, subactivityunit.TITLE subactivityunit, subactivityunit.ID ID from department 
         join contain on department.ID=contain.DEPARTMENT
@@ -29,8 +23,46 @@ class ActivityTree
         where department.ISDELETE=0 and curriculum.ISDELETE=0 and teachingunit.ISDELETE=0 and activityunit.ISDELETE=0 and subactivityunit.ISDELETE=0
         '
         ;
+    /**
+     * Constructeur, recupere le PDO et lance la requete
+     */
+    function __construct()
+    {
+        $this->dbco = DbCo::getPDO();
+        $this->getData();
+    }
 
-        $statement = $this->dbco->query($query);
+/**
+ * Lance la requete et envoi le resultat dans un tableau
+ *
+ * @return void
+ */
+    private function getData()
+    {
+        $statement = $this->dbco->query($this->query);
+        while($item = $statement->fetch(PDO::FETCH_NUM)){
+            array_push($this->data,$item);
+        }
+    }
+
+    /**
+     * Construit les liste a puces imbriquée sur base des elements qui sont different uniquement
+     * 
+     * exemple:
+     * Feuille 1, Feuille2, Feuille3.
+     * Feuille 1, Feuille2, Feuille4.
+     * 
+     * Sera traduit par
+     * Feuille 1
+     *      Feuille2
+     *          Feuille3
+     *          Feuille4
+     *
+     * @param int $retrait le retrait est une façon d'indiqué la fin des valeurs a afficher, 
+     *                      donc que la collones en retrait sera utilisée comme clé dans le lien ou l'appel JS
+     * @return void
+     */
+    function constructTree($retrait=1){
 
         $retrait = 1;
         $limit=null;
@@ -38,15 +70,16 @@ class ActivityTree
         $temp=null;
         $level=-1;
 
-        while($row = $statement->fetch(PDO::FETCH_NUM)){
+       foreach ($this->data as $item) {
+        
             if ($temp==null){
                 $temp=array();
-                $limit=count($row)-1-$retrait;
-                foreach ($row as $key => $value) {
+                $limit=count($item)-1-$retrait;
+                foreach ($item as $key => $value) {
                     $temp[$key]="";
                 }
             }
-            foreach ($row as $key => $value) {
+            foreach ($item as $key => $value) {
                 if ($key<=$limit){
                     if($temp[$key] != $value){
                     $temp[$key] = $value;
@@ -56,13 +89,15 @@ class ActivityTree
                         echo "<ul>";
                     }
                     if ($key<$level){
-                        $level--;
-                        echo "</ul>";
+                        for ($i=$level; $i != $key ; $i--) { 
+                            $level--;
+                            echo "</ul>";
+                        }
                     }
 
                         switch ($key+1) {
                             case ($key+1 == $limit+1):
-                                echo "<a href=\"index.php?index={$row[$limit+1]}\"><li>$temp[$key]</li></a>";
+                                echo "<a onclick=\"request('index.php?index={$item[$limit+1]}')\" href=\"\"><li>$temp[$key]</li></a>";
                                 break;
                             case ($key+1 < $limit+1):
                                 echo "<li>$temp[$key]</li>";
@@ -73,7 +108,7 @@ class ActivityTree
                                 break;
                         }
                     
-                }   
+                     }   
                 }
                 
             }
