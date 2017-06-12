fdd01d5d80e12e1d3c28d4b33ef2fea86c124edd
Phrederik <phrederik2@gmail.com>
2017-06-12 21:17:07 +0200

    week

--- View/WeekView.php
+++ View/WeekView.php
@@ -4,7 +4,9 @@ $str;//="<ul>";
 foreach ($calendar as $key => $value) {
     //if($i == 13){$str.=}
     //"<a onclick=\"request(check,'localId={$value}')\" href=\"#\"><li>$key</a></li>";
-    $str.="<a onclick=\"request(check,'week=".time($value->getMon()*1000)."'\" href=\"#schedule\"><li>Sem.$key({$value->getMon()})</li></a>";
+    $date = strtotime($value->getMon())*1000;
+
+    $str.="<a onclick=\"request(check,'week=".$date."')\" href=\"#schedule\"><li>Sem.$key({$value->getMon()})</li></a>";
     //echo "<br/>Semaine ".$key." ".$value->getMon();
     
 }
